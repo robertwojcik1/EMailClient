112bca47bb72bbf852072be8acd417c9439cb069
robert <robert.wojcik.programista@gmail.com>
2024-11-12 20:35:41 +0100

    Initial commit

---
+++ src/main/java/org/robertwojcik/emailclient/controller/LoginWindowController.java
@@ -0,0 +1,25 @@
+package org.robertwojcik.emailclient;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginWindowController {
+
+    @FXML
+    private TextField emailAddressField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    void loginButtonAction() {
+        System.out.println("clicked!!!");
+    }
+
+}
